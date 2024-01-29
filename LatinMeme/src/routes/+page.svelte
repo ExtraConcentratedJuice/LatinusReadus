e160fa07cb81b2a3adfe09e4c41c924160da1884
ExtraConcentratedJuice <onomatopoeicboom@gmail.com>
2024-01-29 04:24:22 -0800

    yeah

--- LatinMeme/src/routes/+page.svelte
+++ LatinMeme/src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
     import lex from "lexical";
+	import { createEventDispatcher } from "svelte";
 	import { onMount } from "svelte";
 
     let readerDiv;
     let submitButton;
     let textInput;
     let editor;
+    let words = [];
 
     onMount(() => {
 
@@ -19,21 +21,32 @@
         editor.setRootElement(readerDiv);
     })
 
+
     function updateAnalysis(analysis) {
+        words = analysis;
         editor.update(() => {
+            function makeWord(word) {
+                const node = lex.$createTextNode(word.word);
+                node.word = word;
+                return node
+            }
+
             const root = lex.$getRoot();
+
+            root.clear();
+
             const paragraph = lex.$createParagraphNode();
 
             for (let i = 0; i < analysis.length; i++) {
                 const word = analysis[i];
 
                 if (word.pos === "punctuation") {
-                    paragraph.append(lex.$createTextNode(word.word));
+                    paragraph.append(makeWord(word));
                 } else {
                     if (i !== 0) {
                         paragraph.append(lex.$createTextNode(" "));
                     }
-                    paragraph.append(lex.$createTextNode(word.word));
+                    paragraph.append(makeWord(word));
                 }
             }
             root.append(paragraph);
@@ -66,6 +79,21 @@
         </div>
     </div>
     <div class="pure-u-1-3">
-        Words
+        {#each words as word}
+        {#if word.pos !== "punctuation"}
+        <div>
+            <p>Part of Speech: {word.pos}</p>
+            <p>Word: {word.word}</p>
+            <p>Lemma: {word.lemma}</p>
+            <p>Features</p>
+            <ul>
+                {#each Object.entries(word.features) as [name, val]}
+                <li>{name}: {val}</li>
+                {/each}
+            </ul>
+        </div>
+        <hr>
+        {/if}
+        {/each}
     </div>
 </div>
